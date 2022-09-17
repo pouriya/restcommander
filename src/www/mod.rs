fa437f3160cd1a75546af0ab011788b7418f33df
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2022-09-17 18:43:20 +0430

    build: auto generate `src/www/mod.rs` from `build.rs`

--- src/www/mod.rs
+++ src/www/mod.rs
@@ -1,14 +1,4 @@
-pub fn get_index_html() -> String {
-    include_str!("index.html").to_string()
-}
+// This file will be replaced by `build.rs` for each build.
+// After committing this, I will add it to .gitignore to prevent adding auto-generated one in git repo.
 
-pub fn handle_static(uri: String) -> Option<Vec<u8>> {
-    match uri.as_str() {
-        "index.html" => Some(include_bytes!("index.html").to_vec()),
-        "panel.js" => Some(include_bytes!("panel.js").to_vec()),
-        "favicon.ico" => Some(include_bytes!("favicon.ico").to_vec()),
-        "style.css" => Some(include_bytes!("style.css").to_vec()),
-        "inconsolata.woff2" => Some(include_bytes!("inconsolata.woff2").to_vec()),
-        _ => None,
-    }
-}
+pub fn handle_static(uri: String) -> Option<(Vec<u8>, Option<String>)> { None }
