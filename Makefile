787c8be5194e51e533211b11b65ed9f4bad14747
Pouriya <pouriya.jahanbakhsh@gmail.com>
2022-09-17 19:09:19 +0430

    Bootstrap dashboard (#4)
    feat: support Bootstrap 5.2.1

--- Makefile
+++ Makefile
@@ -1,18 +1,22 @@
 DEV_CMD=./target/debug/restcommander
 DEV_DIR=tmp/
 DEV_CFG=${DEV_DIR}config.toml
+BOOTSTRAP_VERSION=$(shell cat www/bootstrap-version.txt)
 
 all: release deb
+	@ ls -sh *.deb
+	@ ls -sh restcommander-*
 
 release:
+	$(eval VERSION=$(shell git describe --tags))
 	cargo build --release
-	cp ./target/release/restcommander .
+	cp ./target/release/restcommander restcommander-${VERSION}
 
 deb: release
 	cargo deb
 	cp ./target/debian/*.deb .
 
-dev:
+dev: download-bootstrap
 	cargo build
 
 setup-dev: dev ${DEV_DIR} ${DEV_CFG}
@@ -20,12 +24,22 @@ setup-dev: dev ${DEV_DIR} ${DEV_CFG}
 start-dev: setup-dev
 	cd ${DEV_DIR} && ../${DEV_CMD} config config.toml
 
+download-bootstrap: www/bootstrap.bundle.min.js www/bootstrap.min.css
+	@ ls -sh www/bootstrap.*
+
+www/bootstrap.bundle.min.js:
+	curl --silent --output www/bootstrap.bundle.min.js https://cdn.jsdelivr.net/npm/bootstrap@${BOOTSTRAP_VERSION}/dist/js/bootstrap.bundle.min.js
+
+www/bootstrap.min.css:
+	curl --silent --output www/bootstrap.min.css https://cdn.jsdelivr.net/npm/bootstrap@${BOOTSTRAP_VERSION}/dist/css/bootstrap.min.css
+
 ${DEV_DIR}:
 	mkdir -p ${DEV_DIR}
 	mkdir -p ${DEV_DIR}www
 	mkdir -p ${DEV_DIR}scripts
 	${DEV_CMD} sample test-script > ${DEV_DIR}/scripts/test && chmod a+x ${DEV_DIR}scripts/test
 	${DEV_CMD} sample test-script-info > ${DEV_DIR}scripts/test.yml
+	cp www/* ${DEV_DIR}www/ && rm -rf ${DEV_DIR}www/bootstrap-version.txt ${DEV_DIR}www/README.md
 
 ${DEV_CFG}:
 	${DEV_CMD} sample config > ${DEV_CFG}.1
@@ -35,8 +49,8 @@ ${DEV_CFG}:
 	awk '$$1 == "tls_cert_file" {$$3="\"cert.pem\""}{print $$0}' ${DEV_CFG}.2 > ${DEV_CFG}.1
 	awk '$$1 == "tls_key_file" {$$3="\"key.pem\""}{print $$0}' ${DEV_CFG}.1 > ${DEV_CFG}.2
 	awk '$$1 == "root_directory" {$$3="\"scripts\""}{print $$0}' ${DEV_CFG}.2 > ${DEV_CFG}.1
-	awk '$$1 == "static_directory" {$$3="\"www\""}{print $$0}' ${DEV_CFG}.1 > ${DEV_CFG}
-	awk '$$1 == "captcha_file" {$$3="\"captcha.txt\""}{print $$0}' ${DEV_CFG}.1 > ${DEV_CFG}
+	awk '$$1 == "static_directory" {$$3="\"www\""}{print $$0}' ${DEV_CFG}.1 > ${DEV_CFG}.2
+	awk '$$1 == "captcha_file" {$$3="\"captcha.txt\""}{print $$0}' ${DEV_CFG}.2 > ${DEV_CFG}
 	rm -rf ${DEV_CFG}.1 ${DEV_CFG}.2
 	${DEV_CMD} sha512 admin > ${DEV_DIR}password-file.sha512
 	${DEV_CMD} sample self-signed-key > ${DEV_DIR}key.pem
@@ -46,19 +60,25 @@ exit-code-status-code-mapping:
 	./tools/exit-code-status-code-mapping
 
 clean:
-	rm -rf ${DEV_DIR}
+	rm -rf ${DEV_DIR} restcommander *.deb *.tar.gz
 
 dist-clean: clean
 	cargo clean
+	rm -rf www/bootstrap.*.js www/bootstrap.*.css
 
 update-self-signed-certificate:
 	openssl genrsa 2048 > src/samples/key.pem
 	echo '\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | openssl req -new -x509 -nodes -days 3650 -key src/samples/key.pem -out src/samples/cert.pem
 
-
 lint:
 	cargo fmt --verbose --check
 	cargo clippy --no-deps
 
+archive:
+	$(eval VERSION=$(shell git describe --tags))
+	$(eval ARCHIVE_GENERIC_EXCLUDE=--exclude='restcommander' --exclude='*.deb' --exclude='*.tar.gz' --exclude='target' --exclude='tmp' --exclude='src/www/*')
+	cd .. && tar ${ARCHIVE_GENERIC_EXCLUDE} -zcvf restcommander-${VERSION}.tar.gz RestCommander && cd RestCommander && mv ../restcommander-${VERSION}.tar.gz .
+	cd .. && tar ${ARCHIVE_GENERIC_EXCLUDE} --exclude='.git' -zcvf restcommander-${VERSION}-src.tar.gz RestCommander && cd RestCommander && mv ../restcommander-${VERSION}-src.tar.gz .
+	ls -sh *.tar.gz
 
 .PHONY: all release deb dev setup-dev start-dev exit-code-status-code-mapping clean dist-clean update-self-signed-certificate lint
---
+++ www/login.js
@@ -0,0 +1,138 @@
+import {ApiOpts, Api} from './api.js'
+import {maybeRemoveElement, setAttributes} from './utils.js'
+
+async function maybeSetupCaptcha() {
+    var captcha = await new Api(ApiOpts).captcha(true)
+    if (captcha === false) {
+        return
+    }
+
+    // Make new Elements:
+    var captchaImageDivElement = document.createElement('div')
+    setAttributes(captchaImageDivElement, {'id': 'captcha-image', 'class': 'form-outline mb-4'})
+    var captchaImageElement = document.createElement('img')
+    setAttributes(
+        captchaImageElement,
+            {
+                'src': 'data:image/png;base64,' + captcha.image,
+                'class': 'img-fluid',
+                'alt': 'CAPTCHA'
+            }
+        )
+    captchaImageDivElement.appendChild(captchaImageElement)
+
+    var captchaRenewElement = document.createElement('button')
+    setAttributes(
+        captchaRenewElement,
+        {
+            'id': 'captcha-renew',
+            'type': 'button',
+            'class': 'btn btn-outline-secondary btn-block mb-4 justify-content-center'
+        }
+    )
+    captchaRenewElement.innerHTML = 'Renew CAPTCHA'
+
+    var captchaIdDivElement = document.createElement('div')
+    setAttributes(captchaIdDivElement, {'id': 'captcha-id', 'class': 'form-outline invisible d-none'})
+    var captchaIdElement = document.createElement('input')
+    setAttributes(
+        captchaIdElement,
+        {
+            'value': captcha.id,
+            'id': 'captcha-id-value',
+            'name': 'captcha-id-value',
+            'class': 'form-outline'
+        }
+    )
+    captchaIdDivElement.appendChild(captchaIdElement)
+
+    var captchaTextDivElement = document.createElement('div')
+    setAttributes(captchaTextDivElement, {'id': 'captcha-text', 'class': 'form-outline mb-4'})
+    var captchaTextElement = document.createElement('input')
+    setAttributes(
+        captchaTextElement,
+        {
+            'type': 'text',
+            'id': 'captcha-text-value',
+            'name': 'captcha-text-value',
+            'class': 'form-control',
+            'placeholder': 'CAPTCHA*',
+            'required': 'required'
+        }
+    )
+    captchaTextDivElement.appendChild(captchaTextElement)
+
+    document.getElementById('captcha-image').replaceWith(captchaImageDivElement)
+    document.getElementById('captcha-renew').replaceWith(captchaRenewElement)
+    document.getElementById('captcha-id').replaceWith(captchaIdDivElement)
+    document.getElementById('captcha-text').replaceWith(captchaTextDivElement)
+    captchaRenewElement.addEventListener('click', captchaRenewClickEventListener)
+}
+
+
+function hideLoginErrorElement() {
+    var loginErrorElement = document.getElementById('login-error')
+    loginErrorElement.innerHTML = ''
+    if (loginErrorElement.className.includes('invisible')) {
+        return
+    }
+    loginErrorElement.className = 'invisible ' + loginErrorElement.className.replace('visible', '')
+}
+
+async function captchaRenewClickEventListener() {
+    event.preventDefault()
+    hideLoginErrorElement()
+    await maybeSetupCaptcha()
+}
+
+async function loginSubmitEventListener(event) {
+    event.preventDefault()
+    hideLoginErrorElement()
+    var inputs = new FormData(event.target);
+    const username = inputs.get('username')
+    const password = inputs.get('password')
+    var captchaId = inputs.get('captcha-id-value')
+    if (captchaId === null) {
+        captchaId = undefined
+    }
+    var captchaText = inputs.get('captcha-text-value')
+    if (captchaText === null) {
+        captchaText = undefined
+    }
+    const authResult = await new Api(ApiOpts).auth(
+        username,
+        password,
+        captchaId,
+        captchaText,
+        async function(result) {
+            if (result.ok) {
+                return true
+            }
+            return result.result
+        }
+    )
+    if (authResult === true) {
+        document.location = 'commands.html'
+        return
+    }
+    var loginErrorElement = document.getElementById('login-error')
+    loginErrorElement.innerHTML = authResult + ''
+    loginErrorElement.className = 'visible ' + loginErrorElement.className.replace('invisible ', '')
+    await maybeSetupCaptcha()
+}
+
+async function main() {
+    const configuration = await new Api(ApiOpts).configuration(true)
+    if (configuration !== false) {
+        if ('serviceName' in configuration) {
+            document.getElementById('login-title').innerHTML = 'Login to ' + configuration.serviceName
+            document.title = configuration.serviceName
+        } else {
+            console.log('Could not found `serviceName` in server configuration')
+        }
+    }
+    document.body.className = 'visible'
+    document.getElementById('login-form').addEventListener('submit', loginSubmitEventListener)
+    maybeSetupCaptcha()
+}
+window.main = main
