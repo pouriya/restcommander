2d76d8a640412e7598f25254f6463afdf571f882
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2022-09-17 18:58:07 +0430

    ref: update cert

--- src/www/mod.rs
+++
@@ -1,4 +0,0 @@
-// This file will be replaced by `build.rs` for each build.
-// After committing this, I will add it to .gitignore to prevent adding auto-generated one in git repo.
-
-pub fn handle_static(uri: String) -> Option<(Vec<u8>, Option<String>)> { None }
