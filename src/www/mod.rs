27ab83574a728eef33bc73f01239672f752e8540
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2022-09-17 19:04:59 +0430

    style: update

--- src/www/mod.rs
+++ src/www/mod.rs
@@ -1,3 +1,5 @@
 // This file will be replaced by `build.rs`
 
-pub fn handle_static(_uri: String) -> Option<(Vec<u8>, Option<String>)> { None }
+pub fn handle_static(_uri: String) -> Option<(Vec<u8>, Option<String>)> {
+    None
+}
