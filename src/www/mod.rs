caa5bae1ebfff2b883a3008ea3a8494771dfdcb0
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2022-09-24 18:44:33 +0330

    init: default version of `src/www/mod.rs`

--- src/www/mod.rs
+++ src/www/mod.rs
@@ -1,4 +1,4 @@
-// This file will be replaced by `build.rs`
+// Will be replaced by `build.rs` based on files in `www` directory
 
 pub fn handle_static(_uri: String) -> Option<(Vec<u8>, Option<String>)> {
     None
