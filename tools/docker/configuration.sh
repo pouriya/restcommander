ef9ce8f59da1b8aa5849cd34839e666530f7ba56
Pouriya Jahanbakhsh <jahanbakhsh@mahsan.co>
2022-09-26 18:31:08 +0330

    build: fix docker listen address

--- tools/docker/configuration.sh
+++ tools/docker/configuration.sh
@@ -7,6 +7,7 @@ mkdir -p /restcommander && cd /restcommander
 mkdir -p scripts
 mkdir -p www
 restcommander sample config > config.toml
+sed -i 's|= \"127.0.0.1\"|= \"0.0.0.0\"|g' config.toml
 restcommander sample self-signed-cert > cert.pem
 restcommander sample self-signed-key > key.pem
 restcommander sha512 admin > password-file.sha512
