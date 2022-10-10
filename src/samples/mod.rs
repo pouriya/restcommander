37d496f95500af7a3efd790e1235c459d668e98a
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2022-10-11 00:20:17 +0330

    ref: build samples

--- src/samples/mod.rs
+++ src/samples/mod.rs
@@ -1,61 +1,12 @@
-use crate::settings::CMDSample;
+// Will be replaced by `build.rs` based on files in `samples` directory
 
-pub fn maybe_print(sample_name: CMDSample) -> Option<String> {
-    match get_sample(sample_name) {
-        Ok(sample_content) => {
-            print!("{}", sample_content);
-            None
-        }
-        Err(reason) => Some(reason),
-    }
-}
+use structopt::StructOpt;
 
-fn get_sample(sample_name: CMDSample) -> Result<String, String> {
-    match sample_name {
-        CMDSample::Config => config(),
-        CMDSample::Python => python(),
-        CMDSample::Shell => shell(),
-        CMDSample::Perl => perl(),
-        CMDSample::SystemdService => systemd_service(),
-        CMDSample::SelfSignedKey => self_signed_key(),
-        CMDSample::SelfSignedCert => self_signed_cert(),
-        CMDSample::TestScript => test_script(),
-        CMDSample::TestScriptInfo => test_script_info(),
-    }
-}
+#[derive(Debug, Clone, StructOpt)]
+#[structopt(about = "Script and configuration samples")]
+pub enum CMDSample {}
 
-fn config() -> Result<String, String> {
-    Ok(include_str!("restcommander.toml").to_string())
-}
-
-fn python() -> Result<String, String> {
-    Ok(include_str!("python.py").to_string())
-}
-
-fn shell() -> Result<String, String> {
-    Ok(include_str!("shell.sh").to_string())
-}
-
-fn perl() -> Result<String, String> {
-    Ok(include_str!("perl.pl").to_string())
-}
-
-fn self_signed_key() -> Result<String, String> {
-    Ok(include_str!("key.pem").to_string())
-}
-
-fn self_signed_cert() -> Result<String, String> {
-    Ok(include_str!("cert.pem").to_string())
-}
-
-fn systemd_service() -> Result<String, String> {
-    Ok(include_str!("restcommander.service").to_string())
-}
-
-fn test_script() -> Result<String, String> {
-    Ok(include_str!("test").to_string())
-}
-
-fn test_script_info() -> Result<String, String> {
-    Ok(include_str!("test.yml").to_string())
+pub fn maybe_print(sample_name: CMDSample) {
+    let sample_data = "There is no sample to print".to_string();
+    println!("{}", sample_data);
 }
