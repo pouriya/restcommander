119b91a1a76cb20a98f5eedcab25366705c7f78a
p <pouriya.jahanbakhsh@gmail.com>
2025-12-22 20:05:24 +0330

    ref: remove background image

--- src/www/mod.rs
+++ src/www/mod.rs
@@ -6,7 +6,6 @@ pub fn handle_static(_uri: String) -> Option<(Vec<u8>, Option<String>)> {
         "commands.html" => Some((include_bytes!("commands.html").to_vec(), Some("text/html".to_string()))),
         "bootstrap.bundle.min.js" => Some((include_bytes!("bootstrap.bundle.min.js").to_vec(), Some("text/javascript".to_string()))),
         "commands.js" => Some((include_bytes!("commands.js").to_vec(), Some("text/javascript".to_string()))),
-        "restcommander-background-image.jpg" => Some((include_bytes!("restcommander-background-image.jpg").to_vec(), None)),
         "login.js" => Some((include_bytes!("login.js").to_vec(), Some("text/javascript".to_string()))),
         "configuration.js" => Some((include_bytes!("configuration.js").to_vec(), Some("text/javascript".to_string()))),
         "styles.css" => Some((include_bytes!("styles.css").to_vec(), Some("text/css".to_string()))),
